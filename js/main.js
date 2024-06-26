55e17c6369d790902c32cefce235b6390b753bdd
Marco <166357601+MarcoNawr@users.noreply.github.com>
2024-06-26 21:23:42 +0200

    Create: Input behavior

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
 /* ##########################################################################
     Globale Variables
 ##########################################################################*/
+const ERRORMESSAGE = "Only Numbers between 2 and 99";
+
 let currentBackgroundcolor = "black";
 let isDrawing = false;
 
 const easDivContainer = document.querySelector(".easDivContainer");
 easDivContainer.addEventListener("mouseleave", () => {isDrawing = false});
+
+const input = document.querySelector(".easContainer__input");
+input.addEventListener("input", ()=>{
+    p_multiplyBy.textContent = "x " + input.value;
+    if(input.value >=2 && input.value <100){
+        p_ErrorMessage.style.visibility = "hidden";
+    }else{
+        p_ErrorMessage.textContent = ERRORMESSAGE;
+        p_ErrorMessage.style.visibility = "visible";
+    }    
+});
+
+const p_ErrorMessage = document.querySelector(".errorMessage");
+const p_multiplyBy = document.querySelector("#p_multiplyBy");
+const btnCreateDivs= document.querySelector("#createDivs");
+btnCreateDivs.addEventListener("click", () =>{
+    if(input.value >2 && input.value <100){
+        createXMultipliedXDivs(input.value);
+    }else{
+        
+    }
+});
 
 /* ##########################################################################
     funktion Deklaration
@@ -46,8 +70,5 @@ function deleteAlleasDivs(){
 }
 
 
-/* ##########################################################################
-    invoke funktions
-##########################################################################*/
-createXMultipliedXDivs(35);
+
 
