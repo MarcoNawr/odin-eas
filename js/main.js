e781b1e198c427777e216f860fccfd7742498b23
Marco <166357601+MarcoNawr@users.noreply.github.com>
2024-06-27 20:21:52 +0200

    create: different Color Modus

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@ const ERRORMESSAGE = "Only Numbers between 2 and 99";
 
 let currentBackgroundcolor = "black";
 let isDrawing = false;
+let isRainbowMode = false;
 
 const easDivContainer = document.querySelector(".easDivContainer");
 easDivContainer.addEventListener("mouseleave", () => {isDrawing = false});
-
+const p_ErrorMessage = document.querySelector(".errorMessage");
+const p_multiplyBy = document.querySelector("#p_multiplyBy");
 const input = document.querySelector(".easContainer__input");
 input.addEventListener("input", ()=>{
     p_multiplyBy.textContent = "x " + input.value;
@@ -19,17 +21,23 @@ input.addEventListener("input", ()=>{
         p_ErrorMessage.style.visibility = "visible";
     }    
 });
-
-const p_ErrorMessage = document.querySelector(".errorMessage");
-const p_multiplyBy = document.querySelector("#p_multiplyBy");
 const btnCreateDivs= document.querySelector("#createDivs");
 btnCreateDivs.addEventListener("click", () =>{
-    if(input.value >2 && input.value <100){
+    if(input.value >=2 && input.value <100){
         createXMultipliedXDivs(input.value);
     }else{
         
     }
 });
+const btnBlack= document.querySelector("#Black");
+btnBlack.addEventListener("click",()=> {setCurrentBackgroundcolor("black"); isRainbowMode=false;});
+const btnBlue= document.querySelector("#Blue");
+btnBlue.addEventListener("click",()=>{setCurrentBackgroundcolor("blue"); isRainbowMode=false;});
+const btnRainbow= document.querySelector("#Rainbow");
+btnRainbow.addEventListener("click",()=> {setCurrentBackgroundcolor("rainbow"); isRainbowMode=true;});
+const btnEraser = document.querySelector("#Eraser");
+btnEraser.addEventListener("click",() => {setCurrentBackgroundcolor("eraser"); isRainbowMode=false;});
+
 
 /* ##########################################################################
     funktion Deklaration
@@ -53,7 +61,12 @@ function createXMultipliedXDivs(x){
             });
             newDiv.addEventListener("mouseover", function(e){
                 if(isDrawing){
-                    e.target.style.backgroundColor = currentBackgroundcolor;
+                    if(isRainbowMode){
+                        setCurrentBackgroundcolor("rainbow");
+                        e.target.style.backgroundColor = currentBackgroundcolor;
+                    }else{
+                        e.target.style.backgroundColor = currentBackgroundcolor;
+                    }
                 }
             });
             newDiv.addEventListener("mouseup", function(){
@@ -69,6 +82,19 @@ function deleteAlleasDivs(){
     easDivContainer.innerHTML= "";
 }
 
+function setCurrentBackgroundcolor(color){
+    if(color === "rainbow"){
+        currentBackgroundcolor =  `rgb(${getRandomInt(255)},${getRandomInt(255)},${getRandomInt(255)})`
+    }else if(color === "eraser"){
+        currentBackgroundcolor =  "rgba( 255, 255, 255, 0.0 )";
+    }else{
+        currentBackgroundcolor = color;
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max+1);
+}
 
 
 
